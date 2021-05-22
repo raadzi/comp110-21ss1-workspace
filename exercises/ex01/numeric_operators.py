3ee252ed248bcab5d971c05de4c6982e4e8289f6
Isabelle Raad <raadzi@email.unc.edu>
2021-05-21 22:22:16 -0400

    Variable Names

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -2,11 +2,11 @@
 
 __author__: str = "730429363"
 
-l = input("Left-hand side: ")
-l: int = int(l)
-r = input("Right-hand side: ")
-r: int = int(r)
-print(str(l) + " ** " + str(r) + " is " + str(l ** r))
-print(str(l) + " / " + str(r) + " is " + str(l / r))
-print(str(l) + " // " + str(r) + " is " + str(l // r))
-print(str(l) + " % " + str(r) + " is " + str(l % r))
+left = input("Left-hand side: ")
+left: int = int(left)
+right = input("Right-hand side: ")
+right: int = int(right)
+print(str(left) + " ** " + str(right) + " is " + str(left ** right))
+print(str(left) + " / " + str(right) + " is " + str(left / right))
+print(str(left) + " // " + str(right) + " is " + str(left // right))
+print(str(left) + " % " + str(right) + " is " + str(left % right))
