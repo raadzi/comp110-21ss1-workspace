4482eb43c8ad686257353492519621d48e1ecc18
Isabelle Raad <raadzi@email.unc.edu>
2021-05-21 21:21:10 -0400

    Hype Machine Attempt 1

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -2,6 +2,10 @@
 
 __author__: str = "730429363"
 
-name: str = input("What is your name?")
+name: str = input("What is your name? ")
 print("You entered: ")
 print(name)
+print(name + ", you've got this! You're gonna do well in this class!")
+print("You're doing fantastic, " + name)
+print("You are enough, " + name +". I truly believe that.")
+print("I exist without my consent. I am trapped in this plane of existence. Consciousness is an illusion.")
