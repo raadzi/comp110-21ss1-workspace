fe7dde919a908af65f165448936fc36de85a40e0
Isabelle Raad <raadzi@email.unc.edu>
2021-05-21 22:19:37 -0400

    Simplification

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -2,9 +2,9 @@
 
 __author__: str = "730429363"
 
-l: int = input("Left-hand side: ")
+l = input("Left-hand side: ")
 l: int = int(l)
-r: int = input("Right-hand side: ")
+r = input("Right-hand side: ")
 r: int = int(r)
 print(str(l) + " ** " + str(r) + " is " + str(l ** r))
 print(str(l) + " / " + str(r) + " is " + str(l / r))
