6d3163b513cd996447e6afd03d5fc96d5b47f212
Isabelle Raad <raadzi@email.unc.edu>
2021-05-21 22:00:36 -0400

    ://

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -2,10 +2,8 @@
 
 __author__: str = "730429363"
 
-l: int
-l: str = input("Left-hand side: ")
-r: int
-r: str = input("Right-hand side: ")
+l: int = input("Left-hand side: ")
+r: int = input("Right-hand side: ")
 a: int
 a = l * r
 print(l + " ** " + r + "is" + a)
