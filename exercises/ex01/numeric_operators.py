6032a6d0dc3d8471dc4a67220644ceaa61a6b27a
Isabelle Raad <raadzi@email.unc.edu>
2021-05-21 22:13:21 -0400

    croissant

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -3,7 +3,7 @@
 __author__: str = "730429363"
 
 l: int = input("Left-hand side: ")
-l: int(l)
+l: int = int(l)
 r: int = input("Right-hand side: ")
-r: int(r)
+r: int = int(r)
 print(l + " ** " + r + "is" + l ** r)
