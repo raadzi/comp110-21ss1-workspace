b3f95620d1f1d3fa1982ca682d65017c93717efe
Isabelle Raad <raadzi@email.unc.edu>
2021-05-24 23:20:51 -0400

    author format

--- exercises/ex02/vaccine_calc.py
+++ exercises/ex02/vaccine_calc.py
@@ -1,6 +1,6 @@
 """A vaccination calculator."""
 
-__author__ = 730429363
+__author__ = "730429363"
 
 # The datetime data type is imported from the datetime library.
 # A datetime object models a specific date and time.
