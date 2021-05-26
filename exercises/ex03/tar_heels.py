182dce622250ae8a0b1109ed78096e90f87f64a4
Isabelle Raad <raadzi@email.unc.edu>
2021-05-25 22:05:22 -0400

    int

--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -3,7 +3,7 @@
 __author__ = "730429363"
 
 
-number: int = input("Enter an int: ")
+number: int = int(input("Enter an int: "))
 if number % 2 == 0:
     if number % 7 == 0:
         print("TAR HEELS")
