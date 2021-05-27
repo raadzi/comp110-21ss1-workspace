603afb32c8680813d1e6dc4827e8e034a999e9d8
Isabelle Raad <raadzi@email.unc.edu>
2021-05-26 21:33:21 -0400

    factorial attempt 2

--- exercises/ex04/factorial.py
+++ exercises/ex04/factorial.py
@@ -4,7 +4,8 @@ __author__ = "730429363"
 
 
 number: int = int(input("Choose a number: "))
+factorial = number
 while number > 1:
     number = number - 1
-    factorial = number * (number + 1)
+    factorial = number * factorial
 print("Factorial: " + str(factorial))
