a84a17d08efd47dca841fdb180402a25684c0817
Isabelle Raad <raadzi@email.unc.edu>
2021-05-27 00:11:44 -0400

    fix

--- exercises/ex04/repeat_beat.py
+++ exercises/ex04/repeat_beat.py
@@ -8,8 +8,9 @@ number: int = int(input("How many times do you want to repeat it? "))
 if number <= 0:
     print("No beat...")
 else:
+    1beat = beat
     while number > 1:
         number -= 1
         beat_space = beat + " "
-        beat = beat_space + beat
+        beat = beat_space + 1beat
     print(beat)
