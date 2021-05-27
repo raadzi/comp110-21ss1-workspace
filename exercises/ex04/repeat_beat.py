7edb156cc014dcb716ef6bd7367d535bece97cbc
Isabelle Raad <raadzi@email.unc.edu>
2021-05-26 21:46:07 -0400

    style

--- exercises/ex04/repeat_beat.py
+++ exercises/ex04/repeat_beat.py
@@ -4,7 +4,7 @@ __author__ = "730429363"
 
 
 beat: str = input("What beat do you want to repeat? ")
-number: int = int(input("How many times do you want to repeat it: "))
+number: int = int(input("How many times do you want to repeat it? "))
 if number <= 0:
     print("No beat...")
 else:
