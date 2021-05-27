57d7b1d4a68771e1ebf8ec2395dba71d021894ab
Isabelle Raad <raadzi@email.unc.edu>
2021-05-26 21:30:38 -0400

    -= to number -

--- exercises/ex04/factorial.py
+++ exercises/ex04/factorial.py
@@ -5,6 +5,6 @@ __author__ = "730429363"
 
 number: int = int(input("Choose a number: "))
 while number > 1:
-    number -= 1
+    number = number - 1
     factorial = number * (number + 1)
 print("Factorial: " + str(factorial))
