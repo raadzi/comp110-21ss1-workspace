448871f19619b7fdf3347cc4f3585a2dde86778b
Isabelle Raad <raadzi@email.unc.edu>
2021-05-26 21:57:26 -0400

    factorial inclusive of 0 and 1

--- exercises/ex04/factorial.py
+++ exercises/ex04/factorial.py
@@ -4,8 +4,11 @@ __author__ = "730429363"
 
 
 number: int = int(input("Choose a number: "))
-factorial = number
-while number > 1:
-    number = number - 1
-    factorial = number * factorial
+if number <= 1:
+    factorial = 1
+else:
+    factorial = number
+    while number > 1:
+      number = number - 1
+      factorial = number * factorial
 print("Factorial: " + str(factorial))
