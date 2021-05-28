68b8581b254a69093c74b07680df8eeb19780edc
Isabelle Raad <raadzi@email.unc.edu>
2021-05-27 23:02:41 -0400

    tarheels(choice)

--- exercises/ex05/tar_heels.py
+++ exercises/ex05/tar_heels.py
@@ -6,17 +6,17 @@ __author__ = "730429363"
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
-    print(tar_heels())
+    print(tar_heels(choice))
     return None
 
-def tar_heels(choice: int) -> str:
-    if choice % 2 == 0:
-        if choice % 7 == 0:
+def tar_heels(number: int) -> str:
+    if number % 2 == 0:
+        if number % 7 == 0:
             return "TAR HEELS"
         else:
             return "TAR"
     else:
-        if choice % 7 == 0:
+        if number % 7 == 0:
             return "HEELS"
         else:
             return "CAROLINA"
