f7cd425312d3b082766c8ae2e73863428cd2ed72
Isabelle Raad <raadzi@email.unc.edu>
2021-05-27 22:52:49 -0400

    im dying

--- exercises/ex05/tar_heels.py
+++ exercises/ex05/tar_heels.py
@@ -6,8 +6,8 @@ __author__ = "730429363"
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
-    tar_heels(choice: int)
     print(tar_heels())
+    tar_heels(choice: int)
     return None
 
 def tar_heels(choice: int) -> str:
