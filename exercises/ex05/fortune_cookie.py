bf66d64437c86cd75e059492d71f741b771081b2
Isabelle Raad <raadzi@email.unc.edu>
2021-05-27 22:09:03 -0400

    return

--- exercises/ex05/fortune_cookie.py
+++ exercises/ex05/fortune_cookie.py
@@ -16,14 +16,14 @@ def fortune_cookie() -> str:
     number: int = int(randint(1, 4))
     if number <= 2:
         if number == 1:
-            "You will take many nice naps this week."
+            return "You will take many nice naps this week."
         else:
-            "Both sides of your pillow will be cold for the next month."
+            return "Both sides of your pillow will be cold for the next month."
     else:
         if number != 4:
-            "You will succeed in Comp 110."
+            return "You will succeed in Comp 110."
         else:
-            "You will have a very slow day at work tomorrow, and no one will yell at you."   
+            return "You will have a very slow day at work tomorrow, and no one will yell at you."   
 
 # Python Idiom for "starting" the program when run as a module.
 # The special dunder variable __name__ will be "__main__" when run as module. 
