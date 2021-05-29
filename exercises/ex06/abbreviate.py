cde9f10c0a97a8be0e163ea39288ff90c37b6bc7
Isabelle Raad <raadzi@email.unc.edu>
2021-05-28 22:20:00 -0400

    lint

--- exercises/ex06/abbreviate.py
+++ exercises/ex06/abbreviate.py
@@ -6,11 +6,12 @@ __author__ = "730429363"
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: str = str(input("Write some text with some uppercase letters: "))
-    print("The abbreviation is \"" + abbreviate(choice) +"\"")
+    print("The abbreviation is \"" + abbreviate(choice) + "\".")
     return None
 
 
 def abbreviate(text: str) -> str:
+    """Function to abbreviate a string of text based on capitalization."""
     position: int = int(-1)
     abbreviation = ""
     while position <= (len(text) - 2):
