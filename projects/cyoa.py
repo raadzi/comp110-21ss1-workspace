2c2dba28deb3548ebf057dee374a9802ec450906
Isabelle Raad <raadzi@email.unc.edu>
2021-06-02 01:45:57 -0400

    f

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -14,7 +14,7 @@ def main() -> None:
     """The entrypoint of the program, when run as a module."""
     greet()
     global points
-    print("Hello, " + player + "! It is a sunny day, and you've just finished watching today's Comp 110 lecture.")
+    print(f"Hello, {player}! It is a sunny day, and you've just finished watching today's Comp 110 lecture.")
     print("You're excited to get started on your homework for the day, but you decide to check your email first.")
     print("You open your inbox and see a message from none other than your university's chancellor himself!")
     print("You read the email and see that Kevin wants to go on a date with you! You... ")
@@ -24,18 +24,18 @@ def main() -> None:
     choice: str = str(input("What do you do? "))
     if choice == "a":
         points += 1
-        print("Understandable. I would do the same. Your Kevin Points are " + str(points) + ".")
+        print(f"Understandable. I would do the same. Your Kevin Points are {points}.")
     elif choice == "b":
         print("Honestly, same. Responding to emails is so laborsome.")
         points += 2
         ignore_email()
-        print("Your Kevin Points are " + str(points) + ".")
+        print(f"Your Kevin Points are {points}.")
     else:
         print("Bestie... are you okay? Did you take your meds today?")
         print("I really hope you selected this one for the meme.")
         points += 5
         points = respond_email(points)
-        print("Your Kevin Points are " + str(points) + ".")
+        print(f"Your Kevin Points are {points}.")
     return None
 
 
@@ -51,7 +51,7 @@ def ignore_email() -> None:
     """Custom procedure call when player ignores kevin's email."""
     global points
     print("After a day, Kevin emails you yet again. The email says...")
-    print("\"" + player + ",")
+    print(f"\"{player},")
     print("It appears that you did not recieve my first email. Would you like to go on a date?\"")
     print("You...")
     print("a) Tell him to stop bothering you. You're not interested.")
@@ -72,7 +72,7 @@ def ignore_email() -> None:
 def respond_email(number: int) -> int:
     """What occurs when you positively respond to kevin's email."""
     print("Kevin quickly responds to your email:")
-    print("\"" + player + ",")
+    print(f"\"{player},")
     print("Perfect! We should meet for dinner at 7 tomorrow!")
     print("Where would you like to go?\"")
     print("a) Chipotle")
