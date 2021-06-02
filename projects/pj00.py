7815ef9b3c8c359f7a456af659097d1daa953643
Isabelle Raad <raadzi@email.unc.edu>
2021-06-01 21:23:02 -0400

    start pj00

---
+++ projects/pj00.py
@@ -0,0 +1,11 @@
+"""A choose your own adventure experience in which you date kevin g."""
+
+__author__ = "730429363"
+
+
+def main() -> None:
+    """The entrypoint of the program, when run as a module."""
+    points: int = int(10)
+    player: str = str(input("What is your name? "))
+    
+    return None
