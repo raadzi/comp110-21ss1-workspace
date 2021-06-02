d90f218cdc94269f4d7fc7efa3a1f72562ea4cea
Isabelle Raad <raadzi@email.unc.edu>
2021-06-01 23:19:46 -0400

    .

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -21,6 +21,7 @@ def greet() -> None:
     return None
 
 def begin_adventure() -> int:
+    global points
     print("Hello, " + player + "! It is a sunny day, and you have just finished watching the latest lecture for your Comp 110 class. You are excited to get started on your homework for the day, but you decide to check your email first. You open your inbox and see a message from none other than your university's chancellor himself!")
     print("You read the email and see that Kevin wants to go on a date with you! You... ")
     print("a) Respond to the email saying that you do not want to date Kevin. This choice ends the dating simulation.")
@@ -28,15 +29,12 @@ def begin_adventure() -> int:
     print("c) Excitedly let Kevin know that you'd love to go on a date with him!")
     choice: str = str(input("What do you do? "))
     if choice == "a":
-        global points
         print("Understandable. I would do the same. Your Kevin Points are " + str(points))
     elif choice == "b":
         print("If you know it's weird, why are you agreeing?? Kinda sus to me tbh.")
-        global points
         points += 1
     else:
         print("Bestie... are you okay? Did you take your meds today? I really hope you selected this one for the meme.")
-        global points
         points += 2
     return int(points)
 
