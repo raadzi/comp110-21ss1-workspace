a6b6406bd4c9bbdadb98d50a99ffd79127959c0a
Isabelle Raad <raadzi@email.unc.edu>
2021-06-01 21:36:39 -0400

    test run

---
+++ projects/projects.cyoa.py
@@ -0,0 +1,13 @@
+"""A choose your own adventure experience in which you date kevin g."""
+
+__author__ = "730429363"
+
+
+def main() -> None:
+    """The entrypoint of the program, when run as a module."""
+    points: int = int(10)
+    player: str = str(input("What is your name? "))
+    return None
+
+if __name__ == "__main__":
+    main()
