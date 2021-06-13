60e18ea77f7975bd559eba9f7d94947d3d2a987a
Isabelle Raad <raadzi@email.unc.edu>
2021-06-13 15:09:20 -0400

    lint

--- exercises/ex07/primes.py
+++ exercises/ex07/primes.py
@@ -5,32 +5,35 @@ __author__ = "7304293963"
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
-    print("This program will evaluate which prime numbers exist between two integers. This includes the first integer, but not the second.")
+    print("This program will evaluate which prime numbers exist between two integers.")
+    print("This includes the first integer, but not the second.")
     first_choice = int(input("Choose your first integer: "))
     second_choice = int(input("Choose your second integer: "))
     print(list_primes(first_choice, second_choice))
 
 
 def is_prime(number: int) -> bool:
+    """Determines whether or not a number is prime."""
     if number <= 1:
         return False
     else:
         x = 2
         remainder: int = number % x
-        while (bool(remainder == 0) == False) and x < number:
+        while (bool(remainder == 0) is False) and x < number:
             x += 1
-        if bool(remainder == 0) == True:
+        if bool(remainder == 0) is True:
             return False
         else:
             return True
 
 
 def list_primes(first_choice: int, second_choice: int) -> list[int]:
+    """Determines whether the numbers between two integers are prime."""
     number = first_choice
     primes: list[int]
     primes = []
     while number < second_choice:
-        if bool(is_prime(number) == True):
+        if bool(is_prime(number)) is True:
             primes.append(number)
         number += 1
     return primes
