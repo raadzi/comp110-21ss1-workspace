4c479ef0f7052375edb9c9d8dbad60e80718eb2a
Isabelle Raad <raadzi@email.unc.edu>
2021-06-16 15:41:28 -0400

    favorite_color attempt 1

--- exercises/ex08/favorite_color.py
+++ exercises/ex08/favorite_color.py
@@ -5,10 +5,21 @@ __author__ = "730429363"
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
-    # TODO 2: Test your functions here
+    students: dict[str, str] = {"Isabelle": "brown", "Mae": "purple", "Riley": "purple", "Jean": "green"}
 
 
-# TODO 1: Define the favorite_color function, and its logic, here.
+def favorite_color(students: dict[str, str]) -> str:
+    colors: list[str, int] = []
+    for student in students:
+        if favorite_color[student] in colors:
+            colors[favorite_color[student]] += 1
+        else:
+            colors.append([favorite_color[student], 1])
+    most_popular: int = colors[0]
+    for color in colors:
+        if color > most_popular:
+            most_popular = color
+    return most_popular
 
 
 if __name__ == "__main__":
