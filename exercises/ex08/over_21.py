0817f7db1cf94edb70a72d58551e3049473b17d7
Isabelle Raad <raadzi@email.unc.edu>
2021-06-16 15:07:10 -0400

    attempt 5 methinks

--- exercises/ex08/over_21.py
+++ exercises/ex08/over_21.py
@@ -6,9 +6,9 @@ __author__ = "730429363"
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     print("Write the names and birth years of the students that you want to know are over 21 or not.")
-    print("Format this entry with their name, followed by a colon, and their birth year.")
+    print("Format this entry with their name in quotes followed by a colon and their birth year.")
     print("Separate each entry with a comma. Eaxample:")
-    print("Isabelle: 2001, Jason: 1999, Caroline: 2002, Riley: 2000")
+    print("\"Isabelle\": 2001, \"Jason\": 1999, \"Caroline\": 2002, \"Riley\": 2000")
     students = dict[str, int](input(""))
     print(over_21(students))
 
