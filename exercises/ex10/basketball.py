32527b1206c923fec221bc48d058e61b561f3c08
Isabelle Raad <raadzi@email.unc.edu>
2021-06-23 13:07:51 -0400

    attempt 1

--- exercises/ex10/basketball.py
+++ exercises/ex10/basketball.py
@@ -1,10 +1,34 @@
 """A class to model a basketball game."""
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730429363"
 
 
-# TODO 1: Define the BBallGame class, and its logic, here.
+class BBallGame:
+    biscuits: bool
+    points: int
+    winning_team: str
+    losing_team: str
 
 
+    def __init__(self, points: int, winning_team: str, losing_team: str):
+        self.biscuits: bool = False
 
 
+    def check_points(self) -> None:
+        if self.points >= 100:
+            self.biscuits: bool = True
+        
+
+    def winner(self) -> str:
+        if self.winning_team == "UNC" and self.losing_team == "Dook":
+            return str("GTHD!!")
+        elif self.winning_team == "UNC":
+            return str("woohoo")
+        else:
+            return str("daggum")
+
+
+    def reset_points(self) -> int:
+        temp = int(self.points)
+        self.points = int(0)
+        return int(temp)
