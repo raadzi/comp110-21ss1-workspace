aae00700a533205abbafaa4d0ec1bd5c17d17d9b
Isabelle Raad <raadzi@email.unc.edu>
2021-06-23 13:18:52 -0400

    attempt 2

--- exercises/ex10/basketball.py
+++ exercises/ex10/basketball.py
@@ -11,15 +11,21 @@ class BBallGame:
 
 
     def __init__(self, points: int, winning_team: str, losing_team: str):
+        """Constructor of the class."""
+        self.points: int = points
+        self.winning_team: str = winning_team
+        self.losing_team: str = losing_team
         self.biscuits: bool = False
 
 
     def check_points(self) -> None:
+        """Checks if there is enough points for biscuits."""
         if self.points >= 100:
             self.biscuits: bool = True
-        
+
 
     def winner(self) -> str:
+        """Return based on whether UNC wins and if it is against Duke."""
         if self.winning_team == "UNC" and self.losing_team == "Dook":
             return str("GTHD!!")
         elif self.winning_team == "UNC":
@@ -29,6 +35,7 @@ class BBallGame:
 
 
     def reset_points(self) -> int:
+        """Resets the points and returns current points."""
         temp = int(self.points)
         self.points = int(0)
         return int(temp)
