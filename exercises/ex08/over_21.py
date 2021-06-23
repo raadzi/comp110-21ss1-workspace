9360053b542fb6735060157dcc20e4266de8e4c0
Isabelle Raad <raadzi@email.unc.edu>
2021-06-23 12:28:56 -0400

    <=

--- exercises/ex08/over_21.py
+++ exercises/ex08/over_21.py
@@ -7,6 +7,6 @@ def over_21(students: dict[str, int]) -> list[str]:
     """Determines which students are over 21."""
     drinking_age: list[str] = []
     for student in students:
-        if students[student] < 2000:
+        if students[student] <= 2000:
             drinking_age.append(student)
     return drinking_age
