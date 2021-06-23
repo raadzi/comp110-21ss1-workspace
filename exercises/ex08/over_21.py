120a37503f26aa08cf951e18112004238264f0cc
Isabelle Raad <raadzi@email.unc.edu>
2021-06-23 12:24:08 -0400

    .

--- exercises/ex08/over_21.py
+++ exercises/ex08/over_21.py
@@ -5,7 +5,6 @@ __author__ = "730429363"
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
-    students: dict[str, int] = {"Isabelle": 2001, "Mae": 1999, "Riley": 2002, "Jean": 2000}
     print(over_21(students))
 
 
