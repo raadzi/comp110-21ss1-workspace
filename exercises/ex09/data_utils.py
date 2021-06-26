4f143b480487a5aaab02acc8c919f6fd0862f333
Isabelle Raad <raadzi@email.unc.edu>
2021-06-26 01:46:52 -0400

    ...

--- exercises/ex09/data_utils.py
+++ exercises/ex09/data_utils.py
@@ -3,7 +3,59 @@
 __author__ = "730429363"
 
 
-#TODO 0: Paste the functions we've been working on in class, here.
+from csv import DictReader
 
 
-# TODO 1: Define the count function, and its logic, here.
+def read_csv_rows(path: str) -> list[dict[str, str]]:
+    """Read a CSV file and return a table that is a list of its rows (dicts)."""
+    file_handle = open(path, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    table: list[dict[str, str]] = []
+    for row in csv_reader:
+        table.append(row)
+    file_handle.close()
+    return table
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Return a column's values."""
+    values: list[str] = []
+    for row in table:
+        values.append(row[column])
+    return values
+
+
+def columnar(table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Convert a table of rows to a table of columns."""
+    result: dict[str, list[str]] = {}
+    keys = table[0].keys()
+    for key in keys:
+        result[key] = column_values(table, key)
+    return result
+
+
+def head(table: dict[str, list[str]], rows: int) -> dict[str, list[str]]:
+    """Get the first n rows."""
+    result: dict[str, list[str]] = {}
+    for key in table:
+        result[key] = table[key][:rows]
+    return result
+
+
+def select(table: dict[str, list[str]], cols: list[str]) -> dict[str, list[str]]:
+    """Select only a subset of columns."""
+    result: dict[str, list[str]] = {}
+    for col_name in cols:
+        result[col_name] = table[col_name]
+    return result
+
+
+def count(values: list[str]) -> dict[str, int]:
+    """Counts the number of times a value is present."""
+    counts: dict[str, int] = []
+    for value in values:
+        if value in counts:
+            counts[value] += 1
+        else:
+            counts[value] = 1
+    return counts
