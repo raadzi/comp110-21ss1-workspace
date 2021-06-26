b38f605156a0bf4396b33d9e9075fd0ae738bba6
Isabelle Raad <raadzi@email.unc.edu>
2021-06-26 00:47:47 -0400

    ,,,

--- projects/pj01/data_utils.py
+++ projects/pj01/data_utils.py
@@ -1,9 +1,9 @@
 from csv import DictReader
 
-file_handle = open("projects/data/survey.csv", encoding = "utf8")
+file_handle = open("data/survey.csv", encoding = "utf8")
 csv_reader = DictReader(file_handle)
 
-table: list[dict[str, str]]
+table: list[dict[str, str]] = []
 for row in csv_reader:
     print(row)
 
