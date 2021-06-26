0756b81bd67e9d54e386325ae8c57dfa4c071967
Isabelle Raad <raadzi@email.unc.edu>
2021-06-26 00:57:59 -0400

    ,,,,

--- projects/pj01/data_utils.py
+++ projects/pj01/data_utils.py
@@ -1,9 +1,15 @@
 from csv import DictReader
 
+
+#opens the data file and creates a data table that is a list of dictionaries (rows)
+#the column titles are the keys for the dictionaries
 file_handle = open("data/survey.csv", encoding = "utf8")
 csv_reader = DictReader(file_handle)
-
 table: list[dict[str, str]] = []
 for row in csv_reader:
-    print(row)
+    table.append(row)
+print(table)
+file_handle.close()
 
+
+#processing a column of the table to calculate the average perception of pace
