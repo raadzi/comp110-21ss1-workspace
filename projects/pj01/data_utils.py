d2c085331f8b2da4ee6a82c34102df6277751cb3
Isabelle Raad <raadzi@email.unc.edu>
2021-06-26 01:40:39 -0400

    data utils

--- projects/pj01/data_utils.py
+++ projects/pj01/data_utils.py
@@ -1,21 +1,50 @@
+"""Data utility functions."""
+
+__author__ = "730429363"
+
+
 from csv import DictReader
 
 
-#opens the data file and creates a data table that is a list of dictionaries (rows)
-#the column titles are the keys for the dictionaries
-file_handle = open("data/survey.csv", encoding = "utf8")
-csv_reader = DictReader(file_handle)
-table: list[dict[str, str]] = []
-for row in csv_reader:
-    table.append(row)
-file_handle.close()
+def read_csv_rows(path: str) -> list[dict[str, str]]:
+    """Read a CSV file and return a table that is a list of its rows (dicts)."""
+    file_handle = open(path, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    table: list[dict[str, str]] = []
+    for row in csv_reader:
+        table.append(row)
+    file_handle.close()
+    return table
 
 
-#processing a column of the table to calculate the average perception of pace
-##creates a list of percieved paces then calculates average pace
-paces: list[float] = []
-for row in table:
-    pace: float = float(row["pace"])
-    paces.append(pace)
-average_pace: float = sum(paces) / len(paces)
-print(f"Average percieved pace: {average_pace}")
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Return a column's values."""
+    values: list[str] = []
+    for row in table:
+        values.append(row[column])
+    return values
+
+
+def columnar(table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Convert a table of rows to a table of columns."""
+    result: dict[str, list[str]] = {}
+    keys = table[0].keys()
+    for key in keys:
+        result[key] = column_values(table, key)
+    return result
+
+
+def head(table: dict[str, list[str]], rows: int) -> dict[str, list[str]]:
+    """Get the first n rows."""
+    result: dict[str, list[str]] = {}
+    for key in table:
+        result[key] = table[key][:rows]
+    return result
+
+
+def select(table: dict[str, list[str]], cols: list[str]) -> dict[str, list[str]]:
+    """Select only a subset of columns."""
+    result: dict[str, list[str]] = {}
+    for col_name in cols:
+        result[col_name] = table[col_name]
+    return result
